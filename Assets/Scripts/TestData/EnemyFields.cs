6ed78fe02b07838ae4fc0213e4d181395a5b577d
コトレ <cottonstar.watahoshi@gmail.com>
2025-05-02 15:29:43 +0900

    feat CSVLoader
    使いやすく改善したローダーの移植

---
+++ Assets/Scripts/TestData/EnemyFields.cs
@@ -0,0 +1,9 @@
+// 扱いたい CSV のヘッダーを定義します。
+// Define CSV headers you want to handle.
+public enum EnemyFields
+{
+    ID,
+    Name,
+    HP,
+    Attack
+}
---
+++ Assets/Scripts/TestData/ScenarioFields.cs
@@ -0,0 +1,18 @@
+﻿// 扱いたい CSV のヘッダーを定義します。
+// Define CSV headers you want to handle.
+public enum ScenarioFields
+{
+    Command,
+    Arg1,
+    Arg2,
+    Arg3,
+    Arg4,
+    Arg5,
+    Arg6,
+    WaitType,
+    Text,
+    PageCtrl,
+    Voice,
+    WindowType,
+    English
+}
